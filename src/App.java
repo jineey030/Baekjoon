1e76af621744efa994fd7a439aa7685dc5a6dad5
jineey <jineey@iteyes.co.kr>
2024-11-05 09:46:28 +0900

    first commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        System.out.println(a+b+c);
+    }
+        
+}
+
