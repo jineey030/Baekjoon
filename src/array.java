448dfbe7993733a7a3f20dfc9d3032c25e5fa198
jineey <ohyejin9816@gmail.com>
2024-11-09 13:30:00 +0900

    [commit] add 행렬 덧셈(2738번)

--- src/array.java
+++ src/array.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class array{
     public static void main(String[] args) throws Exception{
-
         Scanner sc = new Scanner(System.in);
     
         int n = sc.nextInt();
