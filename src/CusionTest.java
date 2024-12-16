f3511aafd589cbd7abbfd12b9e9092e6cd75e7fe
jineey030 <ohyejin9816@gmail.com>
2024-12-16 22:05:35 +0900

    add 촌수계산 (2644번)

--- src/CusionTest.java
+++ src/CusionTest.java
@@ -12,16 +12,13 @@ public class CusionTest {
         a = sc.nextInt() - 1; 
         b = sc.nextInt() - 1; 
         
-        // 부모 자식 관계의 수 m
         m = sc.nextInt();
         
-        // 그래프 초기화
         graph = new ArrayList[n];
         for (int i = 0; i < n; i++) {
             graph[i] = new ArrayList<>();
         }
         
-        // 부모 자식 관계 입력받아서 그래프에 추가
         for (int i = 0; i < m; i++) {
             int parent = sc.nextInt() - 1; 
             int child = sc.nextInt() - 1; 
@@ -45,12 +42,10 @@ public class CusionTest {
             int node = current[0];
             int distance = current[1];
             
-            // 목표 지점에 도달한 경우
             if (node == target) {
                 return distance;
             }
             
-            // 인접 노드들 탐색
             for (int neighbor : graph[node]) {
                 if (!visited[neighbor]) {
                     visited[neighbor] = true;
