040b74ed6de1de5beb26e447df2e68b31349ba33
jineey030 <ohyejin9816@gmail.com>
2024-12-28 17:24:25 +0900

    add 고양이 (10171번)

---
+++ src/CatTest.java
@@ -0,0 +1,17 @@
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class CatTest {
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("\\    /\\"+'\n');
+        bw.write(" )  ( ')"+'\n');
+        bw.write("(  /  )"+'\n');
+        bw.write(" \\(__)|"+'\n');
+
+        bw.flush();
+        bw.close();
+    }
+}
