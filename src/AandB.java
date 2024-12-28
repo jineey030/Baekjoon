5173a24124dc30144fb11525e1740a2b37ea3028
jineey030 <ohyejin9816@gmail.com>
2024-12-28 17:38:05 +0900

    add A/B (1008번)

---
+++ src/AandB.java
@@ -0,0 +1,20 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class AandB {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        double a = Integer.parseInt(st.nextToken());
+        double b = Integer.parseInt(st.nextToken());
+
+        System.out.printf("%.9f", a/b);
+    }
+}
