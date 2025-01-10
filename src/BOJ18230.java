f9f97d6947e131e1c84da7531fc38fba7dad93b3
jineey030 <ohyejin9816@gmail.com>
2025-01-10 22:24:30 +0900

    add 2xN  예쁜 타일링 (18230번)

--- src/BOJ18230.java
+++ src/BOJ18230.java
@@ -1,89 +1,74 @@
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class BOJ18230 {
-    static int n, a, b;
-    static Integer[] arr;
-    static Integer[] brr; 
-
-    static int result = 0;
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        a = Integer.parseInt(st.nextToken());
-        b = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
-        arr = new Integer[a];
-        brr = new Integer[b];
+        Integer[] first = new Integer[a];
+        Integer[] second = new Integer[b];
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < a; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < a; i++) {
+            first[i] = Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < b; i++) {
-            brr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < b; i++) {
+            second[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr, Collections.reverseOrder());
-        Arrays.sort(brr, Collections.reverseOrder());
+        Arrays.sort(first, Collections.reverseOrder());
+        Arrays.sort(second, Collections.reverseOrder());
 
-        int idx1 = 0;
-        int idx2 = 0;
+        int ans = 0;
+        int one = 0;
+        int two = 0;
 
         if(n % 2 == 1){
-            result += arr[idx1];
-            idx1++;
+            ans += first[one];
+            one++;
             n--;
         }
-
+        
         while(n > 0){
-            if(idx1 + 1 >= a){
+            if(one+1 >= a){
                 while(n > 0){
-                    result += brr[idx2];
-                    idx2++;
-                    n -= 2;
+                    ans += second[two];
+                    two++;
+                    n-=2;
                 }
                 break;
             }
 
-            if(idx2 >= b){
+            if(two >= b){
                 while(n > 0){
-                    result += arr[idx1] + brr[idx1 + 1];
-                    idx1 += 2;
-                    n -= 2;
+                    ans += first[one] + first[one+1];
+                    one+=2;
+                    n-=2;
                 }
-
-                break;                   
+                break;
             }
 
-            if(arr[idx1] + arr[idx1 + 1] <= brr[idx2]){
-                result += brr[idx2];
-                idx2++;
-            } else {
-                result += arr[idx1] + arr[idx1 + 1];
-                idx1 += 2;
+            if(first[one] + first[one+1] <= second[two]){
+                ans += second[two];
+                two++;
+            }else{
+                ans+= first[one] + first[one+1];
+                one+=2;
             }
-
-            n -= 2;
+            n-=2;
         }
-      
-        bw.write(result + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
 
+        bw.write(ans+"");
+        br.close();
+        bw.close();
+    }
 }
