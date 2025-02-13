379acaf76f505dff643e6d0433ab0e7d3e114b7e
jineey030 <ohyejin9816@gmail.com>
2025-02-13 14:52:07 +0900

    add 알고리즘 수업 - 알고리즘의 수행 시간 1 (24262번)

--- src/BOJ24262.java
+++ src/BOJ24262.java
@@ -6,7 +6,6 @@ import java.io.OutputStreamWriter;
 
 public class BOJ24262 {
     static int cnt = 0;
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
