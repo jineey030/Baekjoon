8d064d11f2879d7679d574fbff3786a897584d67
jineey030 <ohyejin9816@gmail.com>
2025-02-22 20:43:56 +0900

    add 코딩은 체육과목 입니다 (25314번)

---
+++ src/BOJ25314.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class BOJ25314 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<n; i++){
+            if(n == 4*i){
+                for(int j=0; j<i; j++){
+                    bw.write("long ");
+                }
+                break;
+            }
+        }
+
+        bw.write("int");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
