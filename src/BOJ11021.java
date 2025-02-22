0ab9a619eb306ef92e1c31463508afd42156d7bf
jineey030 <ohyejin9816@gmail.com>
2025-02-22 20:48:45 +0900

    add A+B-7 (11021번)

---
+++ src/BOJ11021.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+class BOJ11021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for(int i=0; i<t; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            bw.write("Case #"+(i+1)+": ");
+            bw.write(a+b+"\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
