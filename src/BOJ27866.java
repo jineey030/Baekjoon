f6ea92413e49958b941b3201996ac3c8ebd92fe9
jineey030 <ohyejin9816@gmail.com>
2025-02-26 15:25:51 +0900

    add 문자와 문자열 (27866번)

---
+++ src/BOJ27866.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class BOJ27866 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char[] arr = br.readLine().toCharArray();
+        int i = Integer.parseInt(br.readLine());
+
+        bw.write(arr[i-1]);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
