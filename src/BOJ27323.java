7be4da8462d59ea86f9f6585b5adcc88c499df92
jineey030 <ohyejin9816@gmail.com>
2025-03-12 15:13:24 +0900

    add 직사각형 (27323번)

---
+++ src/BOJ27323.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class BOJ27323 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(a*b));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
