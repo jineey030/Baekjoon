30873dfc986171d7af25d316093e3836a76b8009
jineey030 <ohyejin9816@gmail.com>
2025-03-14 14:37:45 +0900

    add 알고리즘 수업 - 점근적 표기 1 (24313번)

---
+++ src/BOJ24313.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+class BOJ24313 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a1 = Integer.parseInt(st.nextToken());
+        int a0 = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        if(a1 <= c && (a1 - c) * n + a0 <= 0){
+            bw.write("1");
+        } else {
+            bw.write("0");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
