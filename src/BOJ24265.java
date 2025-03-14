31d37e0f23882c3608afa27d11d78ae5b976dd6e
jineey030 <ohyejin9816@gmail.com>
2025-03-14 14:17:33 +0900

    add 알고리즘 수업 - 알고리즘의 수행 시간 4 (24265번)

---
+++ src/BOJ24265.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class BOJ24265 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long n = Long.parseLong(br.readLine());
+
+        bw.write((n * (n-1)/2) + "\n");
+        bw.write("2");
+        bw.close();
+        br.close();
+    }
+}
