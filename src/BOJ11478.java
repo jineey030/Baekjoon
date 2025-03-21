aae42c6cd5eda2fbf96850e9ddd2a780a9d589d6
jineey030 <ohyejin9816@gmail.com>
2025-03-21 22:01:43 +0900

    add 서로 다른 부분 문자열의 개수 (11478번)

---
+++ src/BOJ11478.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+class BOJ11478 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+
+        Set<String> substrings = new HashSet<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                substrings.add(str.substring(i, j));
+            }
+        }
+
+        bw.write(String.valueOf(substrings.size()));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
